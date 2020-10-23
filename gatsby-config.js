f05463e4ef392b14c07f3519eda02795df1e2a1d
Prasun Kar <prasunkar24@gmail.com>
2020-10-22 23:34:23 -0400

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,33 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Prasun's Starter",
+    titleTemplate: "%s · Prasun's Starter",
+    description: "A simplistic Gatsby starter using TailwindCSS and Sass!",
+    url: "https://www.gatsbyjs.com", // No trailing slash allowed!
+    image: "", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@PrasunKar_",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"),
+        ],
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        custom: {
+          families: ["Inter var", "Inter"],
+          urls: ["./fonts/inter.css"],
+        },
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
 import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const includes = [
+    "SEO component",
+    "Layout component",
+    "Sass",
+    "TailwindCSS",
+    "Inter typeface",
+    "Framer Motion"
+  ]
+
+  return (
+    <Layout>
+      <SEO title="Home" description="This starter uses an SEO component" />
+      <h1>Hello world!</h1>
+      <br />
+
+      <p>This starter includes:</p>
+      {includes.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </Layout>
+  )
 }
